5e417827e4b4765667a2f54c14841cdbd63b4cc5
Samir Makhani <makhani@berkeley.edu>
2013-04-08 03:47:40 -0700

    Changed format of factorial block. again.

--- public/testcode.js
+++ public/testcode.js
@@ -2,7 +2,7 @@ findBlock = function() {
 	var blocks = stage.globalBlocks
 	var len = blocks.length
 	for (var i = 0; i < len; i++) {
-		if (blocks[i].spec == "factorial of %'num'")
+		if (blocks[i].spec == "factorial %'num'")
 			alert("sup");
 			return i;
 	}

536de7015b35b361a4da0c254bd71c2f8694d07c
Samir Makhani <makhani@berkeley.edu>
2013-04-08 03:31:57 -0700

    Change format of factorial block in testcode.js

--- public/testcode.js
+++ public/testcode.js
@@ -2,7 +2,7 @@ findBlock = function() {
 	var blocks = stage.globalBlocks
 	var len = blocks.length
 	for (var i = 0; i < len; i++) {
-		if (blocks[i].spec == "factorial of %'x'")
+		if (blocks[i].spec == "factorial of %'num'")
 			return i;
 	}
 	return null;
